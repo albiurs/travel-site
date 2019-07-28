f5356d967fc9ceba9e937bbef5bc9aa1cd045af6
Urs Albisser <u.albisser@gmail.com>
2019-07-28 17:28:10 +0200

    Section 4.11 - Gulp: include basic function and file watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,55 @@
+/**
+ * import Gulp and Gulp-plugins
+ */
 var gulp = require('gulp');
+//var cleanCSS = require('gulp-clean-css');
 
-gulp.task('default', function(done) {
-  console.log("Hooray, I just created a Gulp task!");
+
+
+/**
+ * define functions
+ */
+// simple console output
+function console_out(done) {
+  console.log("Hooray, I created a Gulp task, just start watching!");
   done();
-});
+};
+
+// process html files
+function process_html(done) {
+  console.log("Imagine something useful being done to your HTML here...");
+  done();
+};
+
+// process css files
+//function process_css(done) {
+//  return gulp.src('styles/**/*.css')
+//    .pipe(cleanCSS())
+//    .pipe(gulp.dest('dist/'));
+
+//  done();
+//};
+function process_css(done) {
+  console.log("Imagine Sass or PostCSS Files running here...");
+  done();
+};
+
+// watch html and css
+function watch(done) {
+  gulp.watch('./app/**/*.html', process_html);
+  gulp.watch('./app/assets/styles/**/*.css', process_css);
+};
+
+
+
+/**
+ * define Gulp tasks:
+ * gulp.task('process_name', execute_process_function);
+ */
+gulp.task('console_out', console_out);
+gulp.task('process_html', process_html);
+gulp.task('process_css', process_css);
+gulp.task('watch', watch);
+
+//default Gulp task
+gulp.task('default', gulp.series(console_out, watch));
