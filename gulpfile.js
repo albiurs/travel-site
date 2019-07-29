97866de89d971078ebbc9a9b7aad987430112b1c
Urs Albisser <u.albisser@gmail.com>
2019-07-29 11:09:43 +0200

    Added Gulp plugins: postcss-nested and gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@
  * import Gulp and Gulp-plugins
  */
 var gulp = require('gulp');
-//var cleanCSS = require('gulp-clean-css'); clean and minify css files
 var postcss = require('gulp-postcss'); // auto-postprocessing of css code
 var autoprefixer = require('autoprefixer'); // prefix css to vendor/browser specific css prefixes
 var cssvars = require('postcss-simple-vars'); // support variables ins css files and convert to standard css code
+var nested = require('postcss-nested'); // support nested commands in css files and convert to standard css code
+var cleanCSS = require('gulp-clean-css'); // clean and minify css files
 
 
 
@@ -14,35 +15,30 @@ var cssvars = require('postcss-simple-vars'); // support variables ins css files
  */
 // simple console output
 function console_out(done) {
-  console.log("Hooray, I created a Gulp task, just start watching!");
-  done();
+	console.log("Hooray, I created a Gulp task, just start watching!");
+	done();
 };
 
 // process html files
 function process_html(done) {
-  console.log("Imagine something useful being done to your HTML here...");
-  done();
+	console.log("Imagine something useful being done to your HTML here...");
+	done();
 };
 
 // process css files
-//function process_css(done) {
-//  return gulp.src('styles/**/*.css')
-//    .pipe(cleanCSS())
-//    .pipe(gulp.dest('dist/'));
-
-//  done();
-//};
 function process_css(done) {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, autoprefixer]))
-    .pipe(gulp.dest('./app/tmp/styles'));
-  done();
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('./app/tmp/styles'));
+	done();
 };
 
 // watch html and css
 function watch(done) {
-  gulp.watch('./app/**/*.html', process_html);
-  gulp.watch('./app/assets/styles/**/*.css', process_css);
+	gulp.watch('./app/**/*.html', process_html);
+	gulp.watch('./app/assets/styles/**/*.css', process_css);
+	done();
 };
 
 
