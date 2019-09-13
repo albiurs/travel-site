753fd0ee8855369e6d962cff7840cfc04cac7000
Urs Albisser <u.albisser@gmail.com>
2019-09-13 13:06:50 +0200

    compleated header basic styling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,13 +2,14 @@
  * import Gulp and Gulp plugins
  */
 var gulp = require('gulp');
-var postcss = require('gulp-postcss'); // auto-postprocessing of css code
-var autoprefixer = require('autoprefixer'); // prefix css to vendor/browser specific css prefixes
-var cssvars = require('postcss-simple-vars'); // support variables ins css files and convert to standard css code
-var nested = require('postcss-nested'); // support nested commands in css files and convert to standard css code
-var cleanCSS = require('gulp-clean-css'); // clean and minify css files
-var cssImport = require('postcss-import'); // import multiple css files into one
-var mixins = require('postcss-mixins'); // reliable handling of media queries
+var postcss = require('gulp-postcss');			// auto-postprocessing of css code
+var autoprefixer = require('autoprefixer');		// prefix css to vendor/browser specific css prefixes
+var cssvars = require('postcss-simple-vars');	// support variables ins css files and convert to standard css code
+var nested = require('postcss-nested');			// support nested commands in css files and convert to standard css code
+var cleanCSS = require('gulp-clean-css');		// clean and minify css files
+var cssImport = require('postcss-import');		// import multiple css files into one
+var mixins = require('postcss-mixins');			// reliable handling of media queries
+var hexrgba = require('postcss-hexrgba');		// transform hex color codes to rba
 
 
 
@@ -18,7 +19,7 @@ var mixins = require('postcss-mixins'); // reliable handling of media queries
 // process css files
 function process_css() {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 		.on('error', function(errorInfo) {
 			console.log(errorInfo.toString()); // output of errors
 			this.emit('end'); // continue watching, even if an error happens
